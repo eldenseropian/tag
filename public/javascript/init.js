975b36e7e78829fa72dad0453aa1eea7d179ee7b
dmetaxak <dmetaxak@cs.brown.edu>
2014-01-25 14:03:16 -0500

    WOW LOOK THERE IS AN END PAGE ALMOST

--- public/javascript/init.js
+++ public/javascript/init.js
@@ -63,6 +63,8 @@ function chooseNextTag(index, tag) {
 function checkIfFinished() {
   if($( '#current' ).text() === $( '#end' ).text() ) {
     console.log("Good job!");
+    $( '#endgame' ).toggleClass('noshow');
+    $( '#hops' ).append(hops);
   }
 }
 
