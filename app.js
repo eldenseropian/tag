6240b2148c135c37180fd3844beecf63be89c10c
Lily Seropian <seropian@mit.edu>
2014-01-24 21:53:45 -0500

    HTML framework set up

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + "/public"));
 
 // development only
 if ('development' == app.get('env')) {
